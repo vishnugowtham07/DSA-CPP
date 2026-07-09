cbd59c501ff2615fc6dc47283bad23f4d9cd49ac
VISHNU K <kit28.24bam062@gmail.com>
2026-07-09 13:32:50 +0530

    Implement removeElement function to filter values

---
+++ Array/Remove Element.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        vector <int> ans;
+        for(int i = 0; i < nums.size() ; i++){
+            if(nums[i] != val)
+                ans.push_back(nums[i]);
+        }
+        nums = ans;
+        return nums.size();
+    }
+};
