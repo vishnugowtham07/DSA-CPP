81a5ac09b9d69d91767ffaf7ce818b79bf0e912a
VISHNU K <kit28.24bam062@gmail.com>
2026-07-09 14:04:36 +0530

    Create Permutations.cpp

---
+++ Recursion/Permutations.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void allpermutation(vector<int>& nums , vector<int>& output , vector<vector<int>>& finaloutput)
+    {
+        if(nums.size() == 0){
+            finaloutput.push_back(output);
+            return;
+        }
+        for(int i = 0; i < nums.size(); i++){
+        
+            int ch = nums[i];
+            vector<int> new_input = nums;
+            new_input.erase(new_input.begin()+i);
+            output.push_back(ch);
+        
+            allpermutation(new_input,output,finaloutput);
+            output.pop_back();
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector< vector<int>> finaloutput;
+        vector <int> output ;
+       // sort(nums.begin(), nums.end());
+        allpermutation(nums,output,finaloutput);
+        return finaloutput;
+    }
+};
